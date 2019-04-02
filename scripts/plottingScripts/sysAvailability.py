d6438be57019d6052219c8f7bfe6812759c7b00a
amjadmajid <amjad.y.majid@gmail.com>
2019-04-02 19:46:02 +0200

    produce system availability figure

--- scripts/plottingScripts/sysAvailability.py
+++ scripts/plottingScripts/sysAvailability.py
@@ -2,17 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-# load data
+plt.style.use('seaborn-whitegrid')
+
+# Load Data 
+path = '../processed_data/availability680.json'
 data=[]
-with open('../processed_data/availability680.json') as f:
+with open(path) as f:
     for l in f:
         data.append(json.loads(l))
 
-for d in data:
-    print(d[0])
-    print(d[1])
-    plt.plot(np.arange(len(d[1]))+1, d[1], label=d[0])
+# Plotting
+## Figure, Axes setup
+fig = plt.figure(figsize=(8,4))
+ax = plt.axes()
+ax.grid(linestyle=":")
 
-plt.legend()
+## Plotting parameters 
+plotPatterns = ['-*', '-^', '-+','-o']
+fontSize = 16 
+
+dataIndices = np.arange(len(data[0][1]))+1 
+
+## Data plotting
+for idx, d in enumerate(data):
+    #print(d[0])
+    print(d[1]) 
+    ax.plot(dataIndices, np.array(d[1])*10,plotPatterns[idx], label=d[0])
+
+## axes formatting 
+ylabels = ["{:4d}%".format(x*10) for x in dataIndices-1]
+ax.set_yticks(dataIndices-1)
+ax.set_yticklabels(ylabels, fontsize=fontSize)
+ax.set_xticklabels(dataIndices, fontsize=fontSize)
+ax.set_xticks(dataIndices)
+
+## Plotting output
+ax.legend(frameon=False, fontsize=fontSize)
+plt.tight_layout()
+plt.savefig('../../paper/figures/sysAvailability.eps')
 plt.show()
 
