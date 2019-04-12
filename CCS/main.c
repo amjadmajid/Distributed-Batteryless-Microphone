fd3b6af29b6815572f8434c3a805b4aaa3d4888d
painter105 <patryk_105@hotmail.com>
2019-04-12 16:17:24 +0200

    Probability desync updated

--- CCS/main.c
+++ CCS/main.c
@@ -136,109 +136,100 @@ void desync_init() {
     TA1CTL = TACLR | TASSEL__ACLK | MC__UP;   // ACLK, counting up
 }
 
-int get_random_bit() {
-    int random_bit = (sampled_input[__randSel++] & 0x01);
+unsigned int get_random_bit() {
+    unsigned int random_bit = (sampled_input[__randSel++] & 0x01);
     if(__randSel >= SAMPLES) __randSel=0;
 
     return random_bit;
 }
 
-int get_random_byte() {
-    int i = 0;
-    if (n>16) n=16;
-    random_bits = get_random_bit();
+unsigned int get_random_int() {
+    int i;
+    unsigned int a;
+    unsigned random_int = get_random_bit();
 
-    for (i ; i < 15; i++) {
-        random_bits = (random_bits << 1) | get_random_bit();
+    for (i=0 ; i < 15; i++) {
+        a = get_random_bit();
+        random_int = (random_int << 1) | a;
     }
 
-    return random_bits;
+    return random_int;
 }
 
 void sleep_for_one_word() {
     // sleep for the time of 1 word
     mic_power_off();
     TA1CCR0 = 13000;  // +-700 ms
+
+    P3OUT &= ~BIT0;
     __bis_SR_register(LPM3_bits | GIE);
+    P3OUT |= BIT0; // <-- DO NOT REMOVE
 
     mic_wait_for_sound(); // will go into normal mode when wakes up.
 }
 
 
-//void desync() {
-//    /*
-//     * Delay recording for de-synchronization
-//     * To be used in a setup with multiple nodes, to prevent all of them reacting to the same event, and miss the next one because of that.
-//     * The random number is based on LSBs from ADC values. P=50% is used
-//     */
-//
-//    while (1) {
-//
-//    //get 2 random bits
-//    int a = get_random_bit();
-//    int b = get_random_bit();
-//    int c = get_random_bit();
-//    int d = (a << 1) | (b << 2) | c;
-//
-//    if (d < 3) {
-//        return;
-//    }
-//
-////    while (sampled_input[__randSel++] & 0x01) {
-////        if(__randSel >= SAMPLES) __randSel=0;
-//        // sleep for the time of 1 word
-//        mic_power_off();
-//        TA1CCR0 = 13000;  // +-700 ms
-//
-//        P3OUT &= ~BIT0;
-//        __bis_SR_register(LPM3_bits | GIE);
-//        P3OUT |= BIT0;
-//
-//        mic_wait_for_sound(); // will go into normal mode when wakes up.
-//    }
-//}
-
 void desync() {
     /*
      * Delay recording for de-synchronization
      * To be used in a setup with multiple nodes, to prevent all of them reacting to the same event, and miss the next one because of that.
-     * The random number is based on LSBs from ADC values. Probability is changed depending on sensed events since last power reset.
+     * The random number is based on LSBs from ADC values. P=50% is used
      */
+    unsigned int p;
 
-    // get 2 random bits
-    int a = get_random_bit();
-    int b = get_random_bit();
-    int c = (a << 1) | b;
-
-    // 1st event with p=25%
-    if (c==0)  {
+    while (1) {
+    p = get_random_int();
+    if (p < (66*65536)/100) {
         return;
     }
-    sleep_for_one_word();
+//    while (sampled_input[__randSel++] & 0x01) {
+//        if(__randSel >= SAMPLES) __randSel=0;
+        // sleep for the time of 1 word
+        mic_power_off();
+        TA1CCR0 = 13000;  // +-700 ms
 
-    // get 2 random bits
-    a = get_random_bit();
-    b = get_random_bit();
-    c = (a << 1) | b;
+        P3OUT &= ~BIT0;
+        __bis_SR_register(LPM3_bits | GIE);
+        P3OUT |= BIT0;
 
-    // 2nd event with p=25%
-    if (c==0)  {
-        return;
+        mic_wait_for_sound(); // will go into normal mode when wakes up.
     }
-    sleep_for_one_word();
-
-    // get 1 random bit
-    c = get_random_bit();
-
-    // 3rd event with p=50%
-    if (c==0)  {
-        return;
-    }
-    sleep_for_one_word();
-
-    // 4th event with p=100%
-
 }
+
+//void desync() {
+//    /*
+//     * Delay recording for de-synchronization
+//     * To be used in a setup with multiple nodes, to prevent all of them reacting to the same event, and miss the next one because of that.
+//     * The random number is based on LSBs from ADC values. Probability is changed depending on sensed events since last power reset.
+//     */
+//
+//    // get 2 random bits
+//    unsigned int p = get_random_int();
+//
+//    // 1st event with p=x%
+//    if (p < (40*65536)/100)  {
+//        return;
+//    }
+//    sleep_for_one_word();
+//    p = get_random_int();
+//
+//    // 2nd event with p=x%
+//    if (p < (50*65536)/100)  {
+//        return;
+//    }
+//    sleep_for_one_word();
+//
+//    p = get_random_int();
+//
+//    // 3rd event with p=x%
+//    if (p < (70*65536)/100)  {
+//        return;
+//    }
+//    sleep_for_one_word();
+//
+//    // 4th event with p=100%
+//
+//}
 
 void compare_init() {
     word_index = NUM_WORDS;
